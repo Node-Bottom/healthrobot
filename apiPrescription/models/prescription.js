68263b0fbb44f11b98304e894f748d561f9fb3d5
kinshuk9449 <57700700+kinshuk9449@users.noreply.github.com>
2021-09-19 18:48:26 +1000

    Update and rename users.js to prescription.js

--- apiPrescription/models/prescription.js
+++ apiPrescription/models/prescription.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 module.exports = mongoose.model('users', new mongoose.Schema({
-  id: String,
-  medicine: String,
+  email: String,
+  prescription: String,
   time: String,
 
 }, { collection : 'health-robot' }));
